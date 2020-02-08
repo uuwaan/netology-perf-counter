e1500f4b4e29b7e173d16b63aa7e20062f90e447
Ivan Shcherbakov <uuwaan@yandex.ru>
2020-02-08 14:58:47 +0300

    Add module with implementations to be measured

---
+++ perms.py
@@ -0,0 +1,30 @@
+import itertools
+
+c_ext = itertools.permutations
+
+
+def pure(iterable, r=None):
+    """
+    Pure-python implementation of itertools.permutations
+    collinwinter@google.com (Collin Winter)
+    """
+    pool = tuple(iterable)
+    n = len(pool)
+    if r is None:
+        r = n
+    indices = list(range(n))
+    cycles = list(range(n - r + 1, n + 1))[::-1]
+    yield tuple(pool[i] for i in indices[:r])
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i + 1:] + indices[i:i + 1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
